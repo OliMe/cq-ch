180c32ca3c56aded012c115a5ae586b8dc5769f8
vizgin <vizgin@sima-land.ru>
2018-10-06 15:45:59 +0500

    Project setup

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Object} command
+ */
+function sendCommand (command) {
+
+}
+/**
+ * @param {Object} query
+ */
+async function sendQuery (query) {
+
+}
+/**
+ * @param {string} type
+ */
+function subscribeCommand (type) {
+
+}
+/**
+ * @param {string} type
+ */
+function subscribeQuery (type) {
+
+}
+export { sendCommand, sendQuery, subscribeCommand, subscribeQuery }
