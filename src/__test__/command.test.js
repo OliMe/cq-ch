d372836a541a735803e9734f524cfbab604755a6
Dmitri Vizgin <vizgin@sima-land.ru>
2019-02-16 16:14:42 +0500

    Add tests for command

--- src/__test__/command.test.js
+++ src/__test__/command.test.js
@@ -3,4 +3,10 @@ describe('command', () => {
     it('creates commandChannel successfully', () => {
         expect(command(['test_type'], 'test')).toBeInstanceOf(Function);
     });
+    it('creates commandChannel unsuccessfully', () => {
+        const createChannelWithoutArguments = () => command()
+        const createChannelWithFirstIncorrectArgument = () => command('not array')
+        expect(createChannelWithoutArguments).toThrowErrorMatchingSnapshot();
+        expect(createChannelWithFirstIncorrectArgument).toThrowErrorMatchingSnapshot();
+    });
 })
